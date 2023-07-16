0f5b43e18c0cdd63aad711a46b4ec59a50174c47
Maurice <maurice@guegan.de>
2023-07-16 03:20:21 +0200

    Readd wifi stuff

--- Software/splitFlapMaster/splitFlapMaster.ino
+++ Software/splitFlapMaster/splitFlapMaster.ino
@@ -6,20 +6,62 @@
 
 // libs
 #include <Arduino.h>
-#include <Wire.h> //for i2c communication
+#include <Wire.h> // for i2c communication
+#include <WiFiEsp.h>
+#include <WiFiEspUdp.h>
+#include <SoftwareSerial.h> // to talk to esp8266 via uart
 
 // Settings for connected units
 const int displayUnits[] = {1, 2}; // array of i2c addresses; Index 0 will get first letter, index 1 the second,...
-#define DISTRIBUTEUNITDELAY 0      // delay between units for letter transmission in ms.
+#define DISTRIBUTEUNITDELAY 500    // delay between units for letter transmission in ms.
 #define BAUDRATE 115200            // baudrate for serial communication
 
-// Settings for UDP server for message transmission
+// WIFI
+SoftwareSerial Serial1(3, 2); // UART to ESP8266
+#define ESPBAUDRATE 9600      // baudrate for ESP8266
+int wifiStatus = WL_IDLE_STATUS;
+const char ssid[] = "TheHorse";       // your network SSID (name)
+const char pass[] = "1nternet";       // your network password, lmao pushing this to github what can go wrong
+WiFiEspUDP Udp;                       // udp instance
+const unsigned int localPort = 10002; // local port to listen on
+const char ReplyBuffer[] = "ACK";     // a string to send back to UDP client after message received
+char packetBuffer[255];               // buffer for received udp message
+
 String currentMessage = "";
 
 void setup()
 {
   // setup Serial
   Serial.begin(BAUDRATE);
+  Serial1.begin(ESPBAUDRATE);
+
+  // initialize ESP module
+  WiFi.init(&Serial1);
+  // check for the presence of the shield
+  if (WiFi.status() == WL_NO_SHIELD)
+  {
+    Serial.println("WiFi shield not present");
+    // don't continue
+    while (true)
+      ;
+  }
+
+  // attempt to connect to WiFi network
+  while (wifiStatus != WL_CONNECTED)
+  {
+    Serial.print("attempting to connect to wpa SSID: ");
+    Serial.println(ssid);
+    wifiStatus = WiFi.begin(ssid, pass); // connect to wpa/wpa2 network
+  }
+
+  Serial.println("you're connected to the network!");
+  IPAddress ip = WiFi.localIP(); // show IP adress
+  Serial.print("IP-Address:");
+  Serial.println(ip);
+
+  Udp.begin(localPort); // start udp server for incoming messages
+  Serial.print("listening on port ");
+  Serial.println(localPort);
 
   Serial.println("starting i2c on master");
   Wire.begin(); // start i2c
@@ -27,9 +69,37 @@ void setup()
 
 void loop()
 {
+  // Input through serial
   if (Serial.available() > 0)
   {
-    String currentMessage = Serial.readString();
+    currentMessage = Serial.readString();
+    Serial.println("(SERIAL) Received a message: " + currentMessage);
+
+    distributeMessageToUnits(currentMessage);
+  }
+
+  // Input through UDP over WiFi
+  int packetSize = Udp.parsePacket();
+  if (packetSize)
+  {
+    Serial.print("(UDP) Received a message: ");
+
+    // Parse packet
+    int len = Udp.read(packetBuffer, 255);
+    if (len > 0)
+    {
+      packetBuffer[len] = 0;
+    }
+
+    // convert bytes to string
+    currentMessage = String((char *)packetBuffer);
+    Serial.println(currentMessage);
+
+    // send a reply, to the IP address and port that sent us the packet we received
+    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+    Udp.write(ReplyBuffer);
+    Udp.endPacket();
+
     distributeMessageToUnits(currentMessage);
   }
 }
@@ -37,9 +107,9 @@ void loop()
 // this function sends message to units
 void distributeMessageToUnits(String message)
 {
-  Serial.println("Showing message: " + message);
-
   message.toUpperCase(); // convert characters to capital characters
+
+  Serial.println("Showing message: " + message);
 
   unsigned int lastParseIndex = 0;
   int amountUnits = sizeof(displayUnits) / sizeof(int);
@@ -142,6 +212,6 @@ void receiveFailure(int adress)
   // unit didn't answer correctly
   Serial.print("Unit ");
   Serial.print(adress);
-  Serial.println(" didn't send an P.");
+  Serial.println(" didn't send a P.");
   // put in some code to handle missing ACK
 }
